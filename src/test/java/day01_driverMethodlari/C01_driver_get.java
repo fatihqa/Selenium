244e2bf892a4fd9fc9b5d32062e452d729c62a44
fatih <gtm3497@gmail.com>
2022-12-20 23:32:31 +0300

    selenium

---
+++ src/test/java/day01_driverMethodlari/C01_driver_get.java
@@ -0,0 +1,20 @@
+package day01_driverMethodlari;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_driver_get {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
+        //windows olanlar sonuda .exe yazmali, mac'de buna gerek yok
+
+        WebDriver driver =new ChromeDriver();
+
+        driver.get("https://www.amazon.com");
+
+        Thread.sleep(5000);
+
+        driver.close();
+    }
+}
